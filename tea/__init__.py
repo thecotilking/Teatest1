c40e35e39d972bfac21a21cb0a8e8e8629a3967c
Easo-boob <18638201989@163.com>
2024-02-23 09:57:21 +0800

    Initial commit

---
+++ tea/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@Project -> File   ：tea -> __init__.py
+@IDE    ：PyCharm
+@Author ：Mr. T
+@Date   ：2024/2/22 17:16
+@Desc   ：kity_stu
+=================================================="""
---
+++ tea/my_awesome_package.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@Project -> File   ：tea -> my_awesome_package
+@IDE    ：PyCharm
+@Author ：Mr. T
+@Date   ：2024/2/22 17:27
+@Desc   ：kity_stu
+=================================================="""
+
+
+def add(a, b):
+  return a + b
